c235b090fc0e40f3e11125d8f82426727e721139
mohnen <markus.mohnen@gmail.com>
2025-01-10 12:13:10 +0100

    .

--- components/harmony-companion-hub/__init__.py
+++ components/harmony-companion-hub/__init__.py
@@ -2,8 +2,12 @@ import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import spi
 from esphome.const import CONF_ID
+from esphome import pins
 
 DEPENDENCIES = ["spi"]
+
+CSN_PIN = "csn_pin"
+CE_PIN = "ce_pin"
 
 harmony_companion_hub_ns = cg.esphome_ns.namespace("harmony_companion_hub")
 HarmonyCompanionHub = harmony_companion_hub_ns.class_(
@@ -11,13 +15,18 @@ HarmonyCompanionHub = harmony_companion_hub_ns.class_(
 )
 
 CONFIG_SCHEMA = (
-    cv.Schema({cv.GenerateID(): cv.declare_id(HarmonyCompanionHub)})
+    cv.Schema({
+        cv.GenerateID(): cv.declare_id(HarmonyCompanionHub),
+        cv.Required(CSN_PIN): pins.gpio_output_pin_schema,
+        cv.Required(CE_PIN): pins.gpio_output_pin_schema
+    })
     .extend(cv.COMPONENT_SCHEMA)
-    .extend(spi.spi_device_schema(cs_pin_required=True))
+    .extend(spi.spi_device_schema(cs_pin_required=False))
 )
 
 
 async def to_code(config):
+
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
     await spi.register_spi_device(var, config)
